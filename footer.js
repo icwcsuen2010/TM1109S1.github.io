33420d500fdaf8e0cfa035f520563942cec7cf04
icwcsuen2010 <81273801+icwcsuen2010@users.noreply.github.com>
2021-03-24 11:36:38 +0800

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,6 +1,6 @@
 document.write(`
   	</div>
-	<script src="https://wadeenghoiwa.github.io/script.js"></script>
+	<script src="https://icwcsuen2010.github.io/TM1109S1.github.io/script.js"></script>
 	<div class="spacer">&nbsp;</div>
 
 <div id="footer">Developed by IC2140 2021 S1 </div>
